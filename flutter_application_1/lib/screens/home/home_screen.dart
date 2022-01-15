86a8a00e1649f3b18baa9a914382980885555386
AJ <adwoa.dadson@ymail.com>
2022-01-15 03:34:43 +0000

    Added homescreen

---
+++ flutter_application_1/lib/screens/home/home_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import  'constants.dart';
+
+class MyHomePage extends StatelessWidget  {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: duplicate_ignore
+    return Scaffold(
+      appBar: buildAppBar(),
+      //body: Body(),
+    );
+  }   
+      
+   AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/back.svg"),
+        onPressed: () {},
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/search.svg",
+            // By default our  icon color is white
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/cart.svg",
+            // By default our  icon color is white
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        // ignore: prefer_const_constructors
+        SizedBox(width: kDefaultPaddin / 2)
+      ],
+    );
+  }
+} 
+    
+  
+  
+  
+}
