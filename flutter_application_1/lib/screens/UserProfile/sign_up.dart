b1bd3c09744343cf3f0cb8adcbe3fc79f0b1f005
AJ <adwoa.dadson@ymail.com>
2022-01-31 12:36:41 +0000

    added signUp

--- flutter_application_1/lib/screens/UserProfile/sign_up.dart
+++ flutter_application_1/lib/screens/UserProfile/sign_up.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_unnecessary_containers, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/UserProfile/sign_up_details.dart';
+//qimport 'package:flutter_application_1/screens/home/home_screen.dart
+
 
 class SignUpPage extends StatelessWidget{
   const SignUpPage({Key? key}) : super(key: key);
@@ -10,6 +13,7 @@ class SignUpPage extends StatelessWidget{
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
+    
     return Scaffold(
       body: Container(
         height: height,
@@ -20,11 +24,62 @@ class SignUpPage extends StatelessWidget{
             children: <Widget>[
                Container(
                  width:width,
-                 height: height*0.30,
-                 decoration: BoxDecoration(color:  Colors.orangeAccent),
+                 height: height*0.45,
+                 decoration: BoxDecoration(
+                   color:  Colors.orangeAccent,
+                   borderRadius: BorderRadius.only(
+                     bottomLeft: Radius.circular(40.0),
+                     bottomRight: Radius.circular(40.0)
+                   ),
+                   
+                   ),
+                child:Center(child: Text('GHANA MALL', style:TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold,
+                color:Colors.white,) ,  
+              ))),
+              SizedBox(height:30.0),
+              
 
-              )
-
+              SizedBox(height:30.0),
+              TextField(
+               decoration: InputDecoration(
+                  hintText: 'Email',
+                  suffixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))
+                  )
+              ),
+              SizedBox(height:20.0),
+              TextField(
+                obscureText:true,
+                decoration: InputDecoration(hintText: 'Password',suffixIcon: Icon(Icons.visibility_off),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))),
+              ),
+               ElevatedButton(
+                child: Text('Login', style: TextStyle(color:Colors.white),),
+                style: ElevatedButton.styleFrom(
+                    //padding: const EdgeInsets.all(20),
+                    primary: Colors.deepOrange
+           ),
+                 onPressed: (){
+                 Navigator.pop(context);
+                  
+                 },
+                  ),
+               SizedBox(height: 20.0,),
+               GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder:(context)=> const SignUpDetails()),); 
+                 },
+                 child: Text.rich(
+                   TextSpan(
+                     text: 'Don\'t have an account? ',
+                     children: [
+                       TextSpan(text: 'SignUp', style:TextStyle(color:Color(0xffEE7B23))
+                     
+                   ),
+                     ]
+                 ),
+                 )
+               )
             ],),
         ),
         
@@ -32,6 +87,9 @@ class SignUpPage extends StatelessWidget{
 
 
  ), 
+    
+    
+    
     );
   }
 
